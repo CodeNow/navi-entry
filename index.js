f9c8558513b1a449ef15292dc0ca7b4c731238d1
Nathan219 <Nathan219@gmail.com>
2016-04-10 20:40:05 -0700

    add @runnable/hostname Adding unit tests for isolation change

--- index.js
+++ index.js
@@ -249,7 +249,7 @@ NaviEntry.prototype.getElasticHostname = function (shortHash) {
     shortHash = this._validateShortHash(shortHash);
   }
   var elasticRe = new RegExp('^frontend:[0-9]+[.]', 'i');
-  var directRe = new RegExp('^frontend:[0-9]+[.]'+shortHash+'-', 'i');
+  var directRe = new RegExp('^frontend:[0-9]+[.]'+shortHash+'-{1,2}', 'i');
   return this.elasticKey ?
     this.elasticKey.replace(elasticRe, ''):
     this.directKey.replace(directRe, '');
